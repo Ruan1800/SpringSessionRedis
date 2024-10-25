8dec178f6f9c546aadc22882e13b611990e20747
Ruan <rrjalfim@gmail.com>
2024-10-24 22:00:34 -0300

    first commit

---
+++ src/main/java/dev/ruan/redi_session/RediSessionApplication.java
@@ -0,0 +1,13 @@
+package dev.ruan.redi_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediSessionApplication.class, args);
+	}
+
+}
